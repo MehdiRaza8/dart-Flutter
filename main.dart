90defa3819a311c21f72710787b9344f47200a44
Mehdi Raza <razamehdi2019@gmail.com>
2022-03-04 00:33:41 -0800

    Assignment

--- main.dart
+++ main.dart
@@ -1,7 +1,23 @@
 void main() {
-  print('Hellow world');
-  print("hellow world");
-  print("hellow");
-  print("now i read");
-  print("watching");
+  num phy = 76;
+  num urdu = 75;
+  num eng = 80;
+  num bio = 59;
+  num che = 88;
+  num obt = (phy + che + eng + urdu + bio);
+  print('Obtain Marks : ${obt}');
+  num total = 500;
+  num per = (obt / total * 100);
+  print("percentage : $per%");
+  if (per >= 90) {
+    print("Grade A+");
+  } else if (per > 85) {
+    print("Grade A+");
+  } else if (per > 70) {
+    print("Grade A");
+  } else if (per > 60) {
+    print("Grade B");
+  } else {
+    print("Grade D");
+  }
 }
